6c2241ed572041f6a7c7d89f5d2a8b0ff8462ddc
Chris Chen <chris@capturedlabs.com>
2025-04-07 20:30:04 +1000

    3.15 - get features and labels data

---
+++ 3.15.x.y.py
@@ -0,0 +1,18 @@
+# 3.11 - data preprocessing
+
+# pandas is used for data manipulation and analysis
+import pandas as pd
+
+
+# import csv to pandas dataframe
+dataset = pd.read_csv("./data/3/Data.csv")
+
+
+features = dataset.iloc[:, :-1].values
+labels = dataset.iloc[:, -1].values
+
+print("--------------------------------features----------------------------")
+print(features)
+
+print("--------------------------------labels----------------------------")
+print(labels)
